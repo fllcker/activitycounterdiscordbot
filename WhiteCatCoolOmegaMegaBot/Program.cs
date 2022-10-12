80aef940f0e2f7ccff2261107ede3def9bd4dc9b
jxwide <96314993+jxwide@users.noreply.github.com>
2022-10-12 23:43:17 +0300

    first commit

---
+++ WhiteCatCoolOmegaMegaBot/Program.cs
@@ -0,0 +1,96 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using WhiteCatCoolOmegaMegaBot.Services;
+
+namespace WhiteCatCoolOmegaMegaBot
+{
+    class Program
+    {
+        DiscordSocketClient client;
+        CommandsService _commandsService;
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            _commandsService = new CommandsService();
+            
+            var config = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.All
+            };
+            client = new DiscordSocketClient(config);
+            client.MessageReceived += CommandsHandler;
+            client.Log += Log;
+
+            var token = "MTAyOTgwOTA2OTczODk2MzAyNA.GY-L8w.FnAzZFLDiIy5MJ9IvcByR2kB1HOZRdzQsFubB8";
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            
+            // voice id = 1029809627044511798
+            // foreach (var user in channel.Users)
+            // {
+            //     Console.WriteLine(user.Username);
+            // }
+
+            Console.ReadLine();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task CommandsHandler(SocketMessage msg)
+        {
+            if (!msg.Author.IsBot)
+            {
+                //Console.WriteLine(msg.Author.Username);
+                //msg.Channel.SendMessageAsync($"{msg.Channel.Name} - {msg.Author} - {msg.Content}");
+                //var user = client.GetUser("precision", "5206");
+                //Console.WriteLine(msg.Content);
+
+                var channel = client.GetChannel(1029809627044511798);
+                var userChannels = channel.Users;
+
+                if (channel.GetChannelType() == ChannelType.Voice)
+                {
+                    Console.Write("ITS VOICE");
+                    var cl = channel as SocketVoiceChannel;
+                    var connectedUsers = cl.ConnectedUsers;
+                    foreach (var cUser in connectedUsers)
+                    {
+                        Console.WriteLine($"user connected: {cUser.Username}");
+                    }
+                }
+                
+                
+                foreach (var oneUser in userChannels)
+                {
+                    var userUsername = oneUser.Username;
+                    if (userUsername != null) Console.WriteLine(userUsername);
+                    
+                    var userActs = oneUser.Activities;
+                    foreach (var act in userActs)
+                    {
+                        Console.WriteLine(act.Name);
+                    }
+                    
+                }
+                
+                
+                // commands
+                var contentText = msg.Content.ToString();
+
+                if (contentText[0] == '/')
+                {
+                    msg.Channel.SendMessageAsync(_commandsService.ExecuteCommand(contentText));
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WhiteCatCoolOmegaMegaBot/Services/CommandsService.cs
@@ -0,0 +1,29 @@
+﻿namespace WhiteCatCoolOmegaMegaBot.Services;
+
+public class CommandsService
+{
+    public string ExecuteCommand(string commandText)
+    {
+        string[] commandAr = commandText.Split(" ");
+        string commandName = commandAr[0].TrimStart('/');
+        commandAr = commandAr.Where(val => val != commandAr[0]).ToArray();
+
+        // commandName - commandName
+        // commandArgs - commandAr[]
+        switch (commandName)
+        {
+            case "random":
+            {
+                return $"Рандомное число (0-1000): {RandomNumber().ToString()}";
+            }
+        }
+
+        return "";
+    }
+
+    public int RandomNumber(int min = 0, int max = 1000)
+    {
+        Random rnd = new Random();
+        return rnd.Next(min, max);
+    }
+}
